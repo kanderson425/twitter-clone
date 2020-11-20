c496e336137366b90700fe53a1b51c380e454bed
kanderson425 <kyleanderson.dev@gmail.com>
2020-11-19 20:57:59 -0600

    Finishes the Sidebar component.'

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,18 +5,28 @@ import SidebarOption from "./SidebarOption";
 import HomeIcon from "@material-ui/icons/Home";
 import SearchIcon from "@material-ui/icons/Search";
 import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
+import MailOutlineIcon from "@material-ui/icons/MapOutlined";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import ListAltIcon from "@material-ui/icons/ListAltOutlined";
+import PerIdentityIcon from "@material-ui/icons/PermIdentityOutlined";
+import MoreHorizIcon from "@material-ui/icons/MoreHorizOutlined";
+import { Button } from "@material-ui/core";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      {/* Twitter ico */}
       <TwitterIcon />
-
-      <SidebarOption Icon={HomeIcon} text="Home" />
+      <SidebarOption active Icon={HomeIcon} text="Home" />
       <SidebarOption Icon={SearchIcon} text="Explore" />
       <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
-      <SidebarOption />
-      <SidebarOption />
+      <SidebarOption Icon={MailOutlineIcon} text="Messagegs" />
+      <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
+      <SidebarOption Icon={ListAltIcon} text="Lists" />
+      <SidebarOption Icon={PerIdentityIcon} text="Profile" />
+      <SidebarOption Icon={MoreHorizIcon} text="More" />
+      <Button variant="outlined" className="sidebar__tweet" fullWidth>
+        Tweet
+      </Button>
     </div>
   );
 }
--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./SidebarOption.css";
 
-function SidebarOption({ text, Icon }) {
+function SidebarOption({ active, text, Icon }) {
   return (
-    <div className="sidebarOption">
-      <h1>{text}</h1>
+    <div className={`sidebarOption ${active && "sidebarOption--active"}`}>
+      <Icon />
+      <h2>{text}</h2>
     </div>
   );
 }
