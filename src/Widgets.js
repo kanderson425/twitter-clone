7afbc15fe6d913bf98938ccf5bcb3d3405d24f58
kanderson425 <kyleanderson.dev@gmail.com>
2020-11-22 13:04:46 -0600

    Adds teh completed, styled out Widget component.

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,32 @@
 import React from "react";
 import "./Widgets.css";
+import SearchIcon from "@material-ui/icons/SearchOutlined";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
 
 function Widgets() {
   return (
     <div className="widgets">
-      <h2>Widgets</h2>
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+      <div className="widgets__widgetContainer">
+        <h2>What's happening</h2>
+        <TwitterTweetEmbed tweetId={"1330258007024922627"} />
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="KyleAnd19005793"
+          options={{ height: 400 }}
+        />
+        <TwitterShareButton
+          url={"https://facebook.com/cleverprogrammer"}
+          options={{ text: "#reactjs is awesome", via: "cleverprogrammer" }}
+        />
+      </div>
     </div>
   );
 }
